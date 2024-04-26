7b7f4ad4441f226743e11a4e1ce25db0d95d176d
zorovhs <tuwibu2021@gmail.com>
2024-04-26 13:49:53 +0700

    Initial commit

---
+++ src/app.ts
@@ -0,0 +1,36 @@
+import * as ProxyChain from 'proxy-chain'
+import * as yargs from 'yargs'
+
+const argv = yargs.options({
+  port: {
+    alias: 'p',
+    description: 'Port to listen on',
+    demandOption: true
+  },
+  forward: {
+    alias: 'f',
+    description: 'Forward proxy: (http|socks5)://user:pass@host:port',
+    demandOption: true
+  }
+}).argv
+
+;(async () => {
+  const { port, forward } = argv as { port: number, forward: string }
+  const server = new ProxyChain.Server({
+    port: 8000,
+    // verbose: true,
+    prepareRequestFunction: () => {
+      return {
+        requestAuthentication: false,
+        upstreamProxyUrl: forward
+      }
+    }
+  })
+  server.listen(() => {
+    console.log(`Proxy server is listening on port ${port}`)
+  })
+  server.on('requestFailed', async ({ request, error }) => {
+    console.error(`Request failed: ${request.url}`)
+    console.error(error)
+  })
+})()
